7a43ce96fc401ff7e803296cb5b01daffb5582c8
Marco Morelli <marco.morelli15@studio.unibo.it>
2026-06-23 19:05:10 +0200

    Move structure of account-service and fix all tests

---
+++ account-service/src/main/java/it/unibo/sap/account/infrastructure/AccountRecord.java
@@ -0,0 +1,23 @@
+package it.unibo.sap.account.infrastructure;
+
+public class AccountRecord {
+
+    public String accountId;
+    public String username;
+    public String passwordHash;
+    public String role;
+    public long whenCreated;
+
+    /** No-arg constructor required by Jackson to deserialize accounts loaded from file. */
+    public AccountRecord() {
+    }
+
+    public AccountRecord(final String accountId, final String username, final String passwordHash,
+                         final String role, final long whenCreated) {
+        this.accountId = accountId;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.role = role;
+        this.whenCreated = whenCreated;
+    }
+}
